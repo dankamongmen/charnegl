f256d07dfd8bfe7952081098ad05a161415f8864
nick black <dankamongmen@gmail.com>
2018-08-06 11:38:23 -0400

    kill off need for explicit template instantiation

--- src/libcharn/configfile.cpp
+++ src/libcharn/configfile.cpp
@@ -4,9 +4,3 @@
 void ConfigFile::loadString(const std::string &s){
 	config.readString(s);
 }
-
-template<typename T> bool ConfigFile::getValue(const std::string &s, T &val){
-	return config.lookupValue(s, val);
-}
-
-template bool ConfigFile::getValue(const std::string &s, std::string &val);
--- src/libcharn/configfile.h
+++ src/libcharn/configfile.h
@@ -5,16 +5,18 @@
 
 class ConfigFile {
 public:
-	ConfigFile() : config() {};
+ConfigFile() : config() {};
 
-	// Load from the specified string.
-	// Propagates libconfig++ parsing exceptions.
-	void loadString(const std::string &s);
+// Load from the specified string.
+// Propagates libconfig++ parsing exceptions.
+void loadString(const std::string &s);
 
-	template<typename T> bool getValue(const std::string &s, T&);
+template<typename T> bool getValue(const std::string &s, T &val){
+	return config.lookupValue(s, val);
+}
 
 private:
-	libconfig::Config config;
+libconfig::Config config;
 };
 
 #endif
