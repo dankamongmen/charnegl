e06d8f7e225258c1ff94ff99d6931548df40d87f
nick black <dankamongmen@gmail.com>
2018-08-11 19:53:22 -0400

    add test using default charnrc

--- src/test/config.cpp
+++ src/test/config.cpp
@@ -1,7 +1,16 @@
 #include <gtest/gtest.h>
 #include "libcharn/config.h"
 
+TEST(CharnConfig, CharnConfigDefaultFile){
+	CharnConfig cf;
+	cf.loadFile("../doc/charnrc"); // FIXME accept as parameter somehow
+	// TODO enumerate things
+	// TODO check lots of crap
+}
+
 TEST(CharnConfig, CharnConfigString){
+	// TODO load the file used in CharnConfigDefaultFile to our own string,
+	// feed it in, use the same detailed checking
 	CharnConfig cf;
 	cf.loadString("crap = \"argh\";");
 	std::string s;
